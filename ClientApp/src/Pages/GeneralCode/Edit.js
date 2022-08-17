59dcef5773bba83563b799d340c3a8def38304b0
biwtiesz <prajakopng4Work@gmail.com>
2022-08-18 00:58:26 +0700

    ToggleActive

--- ClientApp/src/Pages/GeneralCode/Edit.js
+++ ClientApp/src/Pages/GeneralCode/Edit.js
@@ -59,72 +59,76 @@ const Edit = () => {
           {({values, setFieldValue}) => (
             <Form>
               <div className="flex justify-between items-center">
-                <div className="px-4 py-5 sm:px-6">
-                  <h3 className="text-lg leading-6 font-medium text-gray-900">
-                    Edit General Code Detail
-                  </h3>
-                  <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                    General Code Detail
-                  </p>
-                </div>
-                <div className="flex justify-center items-center">
-                  <Switch
-                    checked={values.active}
-                    onChange={() => setFieldValue('active', !values.active)}
-                    className={classNames(
-                      values.active ? 'bg-orange-600' : 'bg-gray-200',
-                      'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500',
-                    )}
-                  >
-                    <span className="sr-only">Use setting</span>
-                    <span
+                <div className="flex justify-between items-center gap-10 px-4 py-5 sm:px-6">
+                  <div>
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      Edit General Code Detail
+                    </h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                      General Code Detail
+                    </p>
+                  </div>
+                  <div>
+                    <Switch
+                      checked={values.active}
+                      onChange={() => setFieldValue('active', !values.active)}
                       className={classNames(
-                        values.active ? 'translate-x-5' : 'translate-x-0',
-                        'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+                        values.active ? 'bg-orange-600' : 'bg-gray-200',
+                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500',
                       )}
                     >
+                      <span className="sr-only">Use setting</span>
                       <span
                         className={classNames(
-                          values.active
-                            ? 'opacity-0 ease-out duration-100'
-                            : 'opacity-100 ease-in duration-200',
-                          'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
+                          values.active ? 'translate-x-5' : 'translate-x-0',
+                          'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
                         )}
-                        aria-hidden="true"
                       >
-                        <svg
-                          className="h-3 w-3 text-gray-400"
-                          fill="none"
-                          viewBox="0 0 12 12"
+                        <span
+                          className={classNames(
+                            values.active
+                              ? 'opacity-0 ease-out duration-100'
+                              : 'opacity-100 ease-in duration-200',
+                            'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
+                          )}
+                          aria-hidden="true"
                         >
-                          <path
-                            d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </span>
-                      <span
-                        className={classNames(
-                          values.active
-                            ? 'opacity-100 ease-in duration-200'
-                            : 'opacity-0 ease-out duration-100',
-                          'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-                        )}
-                        aria-hidden="true"
-                      >
-                        <svg
-                          className="h-3 w-3 text-orange-600"
-                          fill="currentColor"
-                          viewBox="0 0 12 12"
+                          <svg
+                            className="h-3 w-3 text-gray-400"
+                            fill="none"
+                            viewBox="0 0 12 12"
+                          >
+                            <path
+                              d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </span>
+                        <span
+                          className={classNames(
+                            values.active
+                              ? 'opacity-100 ease-in duration-200'
+                              : 'opacity-0 ease-out duration-100',
+                            'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
+                          )}
+                          aria-hidden="true"
                         >
-                          <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
-                        </svg>
+                          <svg
+                            className="h-3 w-3 text-orange-600"
+                            fill="currentColor"
+                            viewBox="0 0 12 12"
+                          >
+                            <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+                          </svg>
+                        </span>
                       </span>
-                    </span>
-                  </Switch>
+                    </Switch>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center">
                   <div className="flex-shrink-0 px-4 py-5 sm:px-6">
                     <div className="flex justify-end space-x-3">
                       {edit == 'edit' ? (
